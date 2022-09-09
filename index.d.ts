07f768c9d8cd11979b8fd5bff421759bebe574fb
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-09-09 20:38:41 +0300

    a

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
-import  { TDBILocaleConstructor, DBILocale } from "@mostfeatured/dbi/dist/types/Locale";
+import  { TDBILocaleConstructor, DBILocale, LangConstructorObject } from "@mostfeatured/dbi/dist/types/Locale";
 
 /**
  * @param locale 
  * @param path path to write the yaml file from locale or locale constructor
  */
-export async function localeToYamlFile(locale: DBILocale | TDBILocaleConstructor, path: string): Promise<void>;
+export function localeToYamlFile(locale: DBILocale | TDBILocaleConstructor<LangConstructorObject>, path: string): Promise<void>;
 
 /**
  * @param yamlPath The Yaml File Path
  * @param localePath path to write "TDBILocaleConstructor as json" from yaml file
  */
-export async function yamlFileToLocaleFile(yamlPath: string, localePath?: string): Promise<TDBILocaleConstructor>;
+export function yamlFileToLocaleFile(yamlPath: string, localePath?: string): Promise<TDBILocaleConstructor<LangConstructorObject>>;
 /**
  * @param yamlPath The Yaml File Path
  */
-export function yamlAsLocale(yamlPath: string, localePath?: string): TDBILocaleConstructor;
+export function yamlAsLocale(yamlPath: string, localePath?: string): TDBILocaleConstructor<LangConstructorObject>;
